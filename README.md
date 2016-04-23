05368227e58c263139b14e9b9d5668925ada5fea
Matt Williams <bleacherbum17+github@gmail.com>
2016-04-23 13:06:12 -0400

    Initial app setup from seed project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# MLM Ionic POC
+# FHIR Hackathon app - Built with Ionic
 - Run `bower install && npm install && ionic setup sass`
 - Run `ionic serve` to start app in browser.
--- www/js/app.js
+++ www/js/app.js
@@ -1,62 +1,63 @@
-(function(){
-  'use strict';
-  angular.module('app', ['ionic', 'ngCordova', 'LocalForageModule'])
-    .config(configure)
-    .run(runBlock);
+( function() {
+    'use strict';
+    angular.module( 'app', [ 'ionic', 'ngCordova', 'LocalForageModule' ] )
+        .config( configure )
+        .run( runBlock );
 
-  configure.$inject = ['$urlRouterProvider', '$provide', '$httpProvider'];
-  function configure($urlRouterProvider, $provide, $httpProvider){
-    // ParseUtilsProvider.initialize(Config.parse.applicationId, Config.parse.restApiKey);
+    configure.$inject = [ '$urlRouterProvider', '$provide', '$httpProvider' ];
 
-    $urlRouterProvider.otherwise('/loading');
+    function configure ( $urlRouterProvider, $provide, $httpProvider ) {
+        // ParseUtilsProvider.initialize(Config.parse.applicationId, Config.parse.restApiKey);
 
-    // improve angular logger
-    $provide.decorator('$log', ['$delegate', 'customLogger', function($delegate, customLogger){
-      return customLogger($delegate);
-    }]);
+        $urlRouterProvider.otherwise( '/loading' );
 
-    // configure $http requests according to authentication
-    $httpProvider.interceptors.push('AuthInterceptor');
-  }
+        // improve angular logger
+        $provide.decorator( '$log', [ '$delegate', 'customLogger', function( $delegate, customLogger ) {
+            return customLogger( $delegate );
+        }]);
 
-  function runBlock($rootScope, $state, $log, AuthSrv, UserSrv, PushPlugin, ToastPlugin, Config){
-    checkRouteRights();
-    setupPushNotifications();
+        // configure $http requests according to authentication
+        $httpProvider.interceptors.push( 'AuthInterceptor' );
+    }
 
-    ////////////////
+    function runBlock( $rootScope, $state, $log, AuthSrv, UserSrv, PushPlugin, ToastPlugin, Config ){
+        checkRouteRights();
+        setupPushNotifications();
 
-    function checkRouteRights(){
-      $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-        if(toState && toState.data && Array.isArray(toState.data.restrictAccess)){
-          var restricted = toState.data.restrictAccess;
-          var logged = AuthSrv.isLogged();
-          if(logged && restricted.indexOf('notLogged') > -1){
-            event.preventDefault();
-            $log.log('IllegalAccess', 'State <'+toState.name+'> is restricted to non logged users !');
-            $state.go('loading');
-          } else if(!logged && restricted.indexOf('logged') > -1){
-            event.preventDefault();
-            $log.log('IllegalAccess', 'State <'+toState.name+'> is restricted to logged users !');
-            $state.go('loading');
-          }
+        ////////////////
+
+        function checkRouteRights () {
+            $rootScope.$on( '$stateChangeStart', function( event, toState, toParams, fromState, fromParams ) {
+                if ( toState && toState.data && Array.isArray( toState.data.restrictAccess ) ) {
+                    var restricted = toState.data.restrictAccess;
+                    var logged = AuthSrv.isLogged();
+                    if ( logged && restricted.indexOf( 'notLogged' ) > -1 ) {
+                        event.preventDefault();
+                        $log.log( 'IllegalAccess', 'State <' + toState.name + '> is restricted to non logged users !');
+                        $state.go('loading');
+                    } else if ( !logged && restricted.indexOf( 'logged' ) > -1 ) {
+                        event.preventDefault();
+                        $log.log( 'IllegalAccess', 'State <' + toState.name + '> is restricted to logged users !' );
+                        $state.go( 'loading' );
+                    }
+                }
+            });
         }
-      });
-    }
 
-    function setupPushNotifications(){
-      // /!\ To use this, you should add Push plugin : ionic plugin add https://github.com/phonegap-build/PushPlugin
-      // registrationId should be uploaded to the server, it is required to send push notification
-      PushPlugin.register(Config.gcm.senderID).then(function(registrationId){
-        return UserSrv.get().then(function(user){
-          if(!user){ user = {}; }
-          user.pushId = registrationId;
-          return UserSrv.set(user);
-        });
-      });
-      PushPlugin.onNotification(function(notification){
-        ToastPlugin.show('Notification received: '+notification.payload.title);
-        console.log('Notification received', notification);
-      });
+        function setupPushNotifications () {
+            // /!\ To use this, you should add Push plugin : ionic plugin add https://github.com/phonegap-build/PushPlugin
+            // registrationId should be uploaded to the server, it is required to send push notification
+            PushPlugin.register( Config.gcm.senderID ).then( function( registrationId ) {
+                return UserSrv.get().then( function( user ) {
+                    if ( !user ) { user = {}; }
+                    user.pushId = registrationId;
+                    return UserSrv.set( user );
+                });
+            });
+            PushPlugin.onNotification( function( notification ) {
+                ToastPlugin.show( 'Notification received: ' + notification.payload.title );
+                console.log( 'Notification received', notification );
+            });
+        }
     }
-  }
 })();
